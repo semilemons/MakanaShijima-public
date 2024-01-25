72f7b6c52bdfdc1c69aee8a846bd7818615480e7
riotamoriya <riotamoriya@gmail.com>
2024-01-26 02:40:39 +0900

    ptry build

---
+++ docs/component---src-pages-index-jsx-fc677f5bfd729b1667d8.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkshijima_makana_publish=self.webpackChunkshijima_makana_publish||[]).push([[230],{1084:function(e,a,n){var t=n(7294);a.Z=e=>{let{children:a}=e;return t.createElement("div",null,a)}},610:function(e,a,n){n.r(a),n.d(a,{Head:function(){return u}});var t=n(7294),l=n(1084);const u=()=>t.createElement(t.Fragment,null,t.createElement("meta",{charSet:"utf-8"}),t.createElement("title",null,"My name is Makana!!"));a.default=()=>t.createElement(l.Z,null)}}]);
+//# sourceMappingURL=component---src-pages-index-jsx-fc677f5bfd729b1667d8.js.map
