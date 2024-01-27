5a3b7000a4a3051ad8c6d9c99c6add370eb7a264
riotamoriya <riotamoriya@gmail.com>
2024-01-27 22:41:48 +0900

    Updates

---
+++ component---src-pages-index-copy-jsx-23d5aef80e05438e1865.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkshijima_makana_publish=self.webpackChunkshijima_makana_publish||[]).push([[191],{1084:function(e,a,n){var t=n(7294);a.Z=e=>{let{children:a}=e;return t.createElement("div",null,a)}},3337:function(e,a,n){n.r(a),n.d(a,{Head:function(){return u}});var t=n(7294),l=n(1084);const u=()=>t.createElement(t.Fragment,null,t.createElement("meta",{charSet:"utf-8"}),t.createElement("title",null,"My name is Makana!!"));a.default=()=>t.createElement(l.Z,null)}}]);
+//# sourceMappingURL=component---src-pages-index-copy-jsx-23d5aef80e05438e1865.js.map
